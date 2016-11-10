e4b0c1ffc1fde4d95a8b880567743fd43aff56ee
quentin <quentin@bannerman.com>
2016-11-10 19:59:34 +0100

    feat(client): ability to expand intervenion vue

--- client/src/components/discussion/DiscussionView.vue
+++ client/src/components/discussion/DiscussionView.vue
@@ -1,8 +1,11 @@
 <template lang="html">
     <div>
-        <intervention-view v-for="intervention in interventions"
+        <intervention-view v-for="intervention in filteredInterventions"
             :intervention="intervention"
             :from-pov="intervenantIsPOV(intervention.intervenant)"></intervention-view>
+        <mdl-button icon @click.native="expanded = true" v-if="!expanded">
+            <i class="material-icons">expand_more</i>
+        </mdl-button>
     </div>
 </template>
 
@@ -11,12 +14,35 @@ import InterventionView from './InterventionView.vue'
 
 export default {
     props: ['interventions', 'pov'],
+    data: ()=>{
+        return {
+            expanded: false
+        }
+    },
     components: {
         InterventionView
     },
     methods: {
         intervenantIsPOV(intervenant) {
             return intervenant && intervenant.id == this.pov.id
+        }
+    },
+    computed: {
+        filteredInterventions(){
+            if(this.expanded) return this.interventions
+
+            const firstIntervention = this.interventions.find(intervention => this.intervenantIsPOV(intervention.intervenant))
+
+            const lightFirstIntervention = Object.assign({}, firstIntervention)
+            let wordsCount = 0
+            lightFirstIntervention.parts = lightFirstIntervention.parts.filter(part => {
+                if(part.type != "text") return false
+                if(wordsCount > 40) return false
+                wordsCount += part.content.split(' ').length
+                return true
+            })
+
+            return [lightFirstIntervention]
         }
     }
 }
